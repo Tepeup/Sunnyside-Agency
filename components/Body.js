fdb03bde1f86e78e4d069bf69914289f00d3dd97
Your Name <you@example.com>
2021-06-11 19:59:30 -0500

    Set up initial page strucutre and imported images

---
+++ components/Body.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Body() {
+  return (
+    <div class="container">
+      <div class="Header"></div>
+      <div class="Testimonials"></div>
+      <div class="Footer"></div>
+      <div class="One"></div>
+      <div class="Two"></div>
+      <div class="Three"></div>
+      <div class="Four"></div>
+      <div class="Five"></div>
+      <div class="Six"></div>
+      <div class="Seven"></div>
+      <div class="Eight"></div>
+    </div>
+  );
+}
